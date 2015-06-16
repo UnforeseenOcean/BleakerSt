a331d3b04a029c32058b4b45823cacd97d24c217
Ansh <lightnarcissus@gmail.com>
2015-06-15 20:08:41 -0400

    Initial Commit

---
+++ Bleaker St/Assets/VideoPlay.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class VideoPlay : MonoBehaviour {
+
+	// Use this for initialization
+    public MovieTexture movTexture;
+	void Start () {
+
+        //renderer.material.mainTexture = movTexture;
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        movTexture.Play();
+
+	
+	}
+}
